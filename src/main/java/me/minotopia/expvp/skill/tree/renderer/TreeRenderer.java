b81e3e39b5e828ed080168374628f3b15f41d56e
xxyy <devnull@nowak-at.net>
2016-06-24 22:42:18 +0200

    Clarify name casing

---
+++ src/main/java/me/minotopia/expvp/skill/tree/renderer/TreeRenderer.java
@@ -0,0 +1,22 @@
+/*
+ * This file is part of ExPvP,
+ * Copyright (c) 2016-2016.
+ *
+ * This work is protected by international copyright laws and licensed
+ * under the license terms which can be found at src/main/resources/LICENSE.txt.
+ */
+
+package me.minotopia.expvp.skill.tree.renderer;
+
+import io.github.xxyy.common.inventory.gui.holder.TemplateElementHolder;
+
+/**
+ * Capable of rendering a skill tree into cache and creating inventories that show it.
+ *
+ * @author <a href="http://xxyy.github.io/">xxyy</a>
+ * @since 2016-06-24
+ */
+public class TreeRenderer extends TemplateElementHolder {
+    //private final EPPlugin plugin;
+    //private final SkillTree tree;
+}
