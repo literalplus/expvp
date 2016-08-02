ae9dbdc31411fbf0d6ebc1efd6d94f4852fadb8f
xxyy <devnull@nowak-at.net>
2016-08-02 22:48:07 +0200

    Set German Locale on XYC-Intake

--- src/main/java/me/minotopia/expvp/EPPlugin.java
+++ src/main/java/me/minotopia/expvp/EPPlugin.java
@@ -31,6 +31,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.List;
+import java.util.Locale;
 import java.util.UUID;
 
 /**
@@ -88,6 +89,7 @@ public class EPPlugin extends GenericXyPlugin {
 
     private void registerCommands() {
         commandsManager = new CommandsManager(this);
+        commandsManager.getErrorTranslator().setLocale(Locale.GERMAN);
         registerInjections();
         commandsManager.registerCommand(new CommandSkillAdmin(), "skilladmin", "ska");
     }
