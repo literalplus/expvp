84d1bc5a14fcc805eada523711b5fcdae5093921
Literallie <devnull@nowak-at.net>
2017-02-21 13:51:36 +0100

    Use an actual map in SimpleHandlerMap instead of a plain Collection

--- src/main/java/me/minotopia/expvp/handler/SimpleHandlerMap.java
+++ src/main/java/me/minotopia/expvp/handler/SimpleHandlerMap.java
@@ -13,10 +13,10 @@ import me.minotopia.expvp.api.handler.HandlerMap;
 import me.minotopia.expvp.api.handler.SkillHandler;
 import me.minotopia.expvp.skill.meta.Skill;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.function.Predicate;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
@@ -26,42 +26,39 @@ import java.util.stream.Collectors;
  * @since 2016-09-17
  */
 public class SimpleHandlerMap<T extends SkillHandler> implements HandlerMap<T> {
-    private final Collection<T> handlers = new ArrayList<>();
+    private final Map<Skill, T> handlers = new HashMap<>();
 
     @Override
     public void registerHandler(T handler) {
         Preconditions.checkNotNull(handler, "handler");
         Preconditions.checkNotNull(handler.getSkill(), "handler.getSkill()");
-        handlers.add(handler);
+        handlers.put(handler.getSkill(), handler);
     }
 
     @Override
     public void unregisterHandler(T handler) {
         Preconditions.checkNotNull(handler, "handler");
-        handlers.remove(handler);
+        Preconditions.checkNotNull(handler.getSkill(), "handler.getSkill()");
+        handlers.remove(handler.getSkill(), handler);
     }
 
     @Override
     public void unregisterHandlers(Skill skill) {
         Preconditions.checkNotNull(skill, "skill");
-        handlers.removeIf(handler -> handler.getSkill().equals(skill));
+        handlers.remove(skill);
     }
 
     @Override
     public Collection<T> getAllHandlers() {
-        return Collections.unmodifiableCollection(handlers);
-    }
-
-    public Collection<T> getMatchingHandlers(Predicate<? super T> filter) {
-        Preconditions.checkNotNull(filter, "filter");
-        return handlers.stream()
-                .filter(filter)
-                .collect(Collectors.toList());
+        return Collections.unmodifiableCollection(handlers.values());
     }
 
     @Override
     public Collection<T> getRelevantHandlers(Collection<? extends Skill> skills) {
         Preconditions.checkNotNull(skills, "skills");
-        return getMatchingHandlers(handler -> skills.contains(handler.getSkill()));
+        return handlers.entrySet().stream()
+                .filter(e -> skills.contains(e.getKey()))
+                .map(Map.Entry::getValue)
+                .collect(Collectors.toList());
     }
 }
