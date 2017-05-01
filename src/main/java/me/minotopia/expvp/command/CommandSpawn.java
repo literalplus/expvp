b48ff88d21567541507c614cb39a6eb9110e7c1d
Literallie <git@l1t.li>
2017-05-01 17:49:31 +0200

    Hack that overrides xLogin's /spawn command
    fuck you bukkit
    seriously

--- src/main/java/me/minotopia/expvp/command/CommandSpawn.java
+++ src/main/java/me/minotopia/expvp/command/CommandSpawn.java
@@ -9,16 +9,23 @@
 package me.minotopia.expvp.command;
 
 import com.google.inject.Inject;
-import com.sk89q.intake.Command;
-import li.l1t.common.intake.provider.annotation.Sender;
+import li.l1t.common.command.BukkitExecution;
+import li.l1t.common.command.BukkitExecutionExecutor;
+import li.l1t.common.exception.InternalException;
+import li.l1t.common.exception.UserException;
 import li.l1t.common.util.LocationHelper;
 import li.l1t.common.util.task.TaskService;
+import li.l1t.xlogin.common.api.XLoginProfile;
+import me.minotopia.expvp.EPPlugin;
+import me.minotopia.expvp.api.misc.ConstructOnEnable;
 import me.minotopia.expvp.api.spawn.SpawnService;
 import me.minotopia.expvp.i18n.I18n;
+import me.minotopia.expvp.logging.LoggingManager;
+import org.apache.logging.log4j.Logger;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
 
-import java.io.IOException;
 import java.time.Duration;
 
 /**
@@ -27,21 +34,37 @@ import java.time.Duration;
  * @author <a href="https://l1t.li/">Literallie</a>
  * @since 2016-07-23
  */
-@AutoRegister("spawn")
-public class CommandSpawn {
+@ConstructOnEnable
+public class CommandSpawn extends BukkitExecutionExecutor {
+    private static final Logger LOGGER = LoggingManager.getLogger(CommandSpawn.class);
     private final SpawnService spawns;
     private final TaskService tasks;
 
     @Inject
-    public CommandSpawn(SpawnService spawns, TaskService tasks) {
+    public CommandSpawn(SpawnService spawns, TaskService tasks, EPPlugin plugin) {
         this.spawns = spawns;
         this.tasks = tasks;
+        plugin.getCommand("spawn").setExecutor(this);
+        overrideXLoginSpawnCommand(plugin);
     }
 
-    @Command(aliases = "",
-            desc = "cmd!spawn.root.desc")
-    public void newSkill(@Sender Player player)
-            throws IOException {
+    private void overrideXLoginSpawnCommand(EPPlugin plugin) {
+        try {
+            if (plugin.getConfig().getBoolean("override-xlogin-spawn-command-because-bukkits-command-handling-sucks", true)) {
+                JavaPlugin xLogin = JavaPlugin.getProvidingPlugin(XLoginProfile.class);
+                if (xLogin != null) {
+                    LOGGER.debug("Overriding xLogin's /spawn because Bukkit's command registration sucks...");
+                    xLogin.getCommand("spawn").setExecutor(this);
+                }
+            }
+        } catch (Exception e) {
+            LOGGER.info("Failed to override xLogin's /spawn", e);
+        }
+    }
+
+    @Override
+    public boolean execute(BukkitExecution exec) throws UserException, InternalException {
+        Player player = exec.player();
         I18n.sendLoc(player, "core!spawn.stand-still");
         Location initialLocation = player.getLocation();
         double initialHealth = player.getHealth();
@@ -55,5 +78,6 @@ public class CommandSpawn {
                 spawns.teleportToSpawnIfPossible(player);
             }
         }, Duration.ofSeconds(5));
+        return true;
     }
 }
