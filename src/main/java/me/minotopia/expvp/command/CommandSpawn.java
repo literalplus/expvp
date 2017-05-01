debd3466446458fe3c78a490124fd3cd05356673
Literallie <git@l1t.li>
2017-05-01 17:23:40 +0200

    Add a five second delay to /spawn; Fixes #816

--- src/main/java/me/minotopia/expvp/command/CommandSpawn.java
+++ src/main/java/me/minotopia/expvp/command/CommandSpawn.java
@@ -11,10 +11,15 @@ package me.minotopia.expvp.command;
 import com.google.inject.Inject;
 import com.sk89q.intake.Command;
 import li.l1t.common.intake.provider.annotation.Sender;
+import li.l1t.common.util.LocationHelper;
+import li.l1t.common.util.task.TaskService;
 import me.minotopia.expvp.api.spawn.SpawnService;
+import me.minotopia.expvp.i18n.I18n;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
 import java.io.IOException;
+import java.time.Duration;
 
 /**
  * A command that teleports to spawn.
@@ -25,16 +30,30 @@ import java.io.IOException;
 @AutoRegister("spawn")
 public class CommandSpawn {
     private final SpawnService spawns;
+    private final TaskService tasks;
 
     @Inject
-    public CommandSpawn(SpawnService spawns) {
+    public CommandSpawn(SpawnService spawns, TaskService tasks) {
         this.spawns = spawns;
+        this.tasks = tasks;
     }
 
     @Command(aliases = "",
             desc = "cmd!spawn.root.desc")
     public void newSkill(@Sender Player player)
             throws IOException {
-        spawns.teleportToSpawnIfPossible(player);
+        I18n.sendLoc(player, "core!spawn.stand-still");
+        Location initialLocation = player.getLocation();
+        double initialHealth = player.getHealth();
+        int initialRemainingAir = player.getRemainingAir();
+        tasks.delayed(() -> {
+            if (!LocationHelper.softEqual(initialLocation, player.getLocation()) ||
+                    player.getHealth() < initialHealth ||
+                    player.getRemainingAir() < initialRemainingAir) {
+                I18n.sendLoc(player, "core!spawn.tp-abort");
+            } else {
+                spawns.teleportToSpawnIfPossible(player);
+            }
+        }, Duration.ofSeconds(5));
     }
 }
