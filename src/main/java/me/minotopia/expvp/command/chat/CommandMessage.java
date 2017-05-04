efa2bde15158de56428aaca89efe8121e9ac3ab6
Literallie <git@l1t.li>
2017-05-05 00:07:37 +0200

    Refactor /msg to BukkitExecution because Intake sucks at ""

--- src/main/java/me/minotopia/expvp/command/chat/CommandMessage.java
+++ src/main/java/me/minotopia/expvp/command/chat/CommandMessage.java
@@ -9,12 +9,19 @@
 package me.minotopia.expvp.command.chat;
 
 import com.google.inject.Inject;
-import com.sk89q.intake.Command;
-import li.l1t.common.intake.provider.annotation.Colored;
-import li.l1t.common.intake.provider.annotation.Merged;
-import li.l1t.common.intake.provider.annotation.OnlineSender;
+import li.l1t.common.command.BukkitExecution;
+import li.l1t.common.command.BukkitExecutionExecutor;
+import li.l1t.common.exception.InternalException;
+import li.l1t.common.exception.UserException;
+import li.l1t.common.util.UUIDHelper;
+import me.minotopia.expvp.EPPlugin;
 import me.minotopia.expvp.api.chat.message.PMService;
-import me.minotopia.expvp.command.AutoRegister;
+import me.minotopia.expvp.api.misc.ConstructOnEnable;
+import me.minotopia.expvp.api.misc.PlayerService;
+import me.minotopia.expvp.i18n.I18n;
+import me.minotopia.expvp.i18n.exception.I18nInternalException;
+import me.minotopia.expvp.i18n.exception.I18nUserException;
+import org.bukkit.Server;
 import org.bukkit.command.CommandSender;
 
 /**
@@ -23,17 +30,48 @@ import org.bukkit.command.CommandSender;
  * @author <a href="https://l1t.li/">Literallie</a>
  * @since 2017-05-04
  */
-@AutoRegister(value = "msg", aliases = {"whisper", "tell", "t", "m"})
-public class CommandMessage {
+@ConstructOnEnable
+public class CommandMessage extends BukkitExecutionExecutor {
     private final PMService pmService;
+    private final PlayerService playerService;
+    private final Server server;
 
     @Inject
-    public CommandMessage(PMService pmService) {
+    public CommandMessage(PMService pmService, EPPlugin plugin, PlayerService playerService, Server server) {
         this.pmService = pmService;
+        this.playerService = playerService;
+        this.server = server;
+        plugin.getCommand("msg").setExecutor(this);
     }
 
-    @Command(aliases = "", desc = "cmd!msg.root.desc", min = 2)
-    public void root(CommandSender sender, @OnlineSender CommandSender receiver, @Merged @Colored String message) {
-        pmService.sendMessage(sender, receiver, message);
+    @Override
+    public boolean execute(BukkitExecution exec) throws UserException, InternalException {
+        try {
+            handle(exec);
+        } catch (I18nUserException | I18nInternalException e) {
+            I18n.sendLoc(exec.sender(), e.toMessage());
+        }
+        return true;
+    }
+
+    private void handle(BukkitExecution exec) {
+        if (exec.args().length < 2) {
+            I18n.sendLoc(exec.sender(), "cmd!msg.usage");
+        } else {
+
+            pmService.sendMessage(exec.sender(), findReceiver(exec.arg(0)), exec.joinedArgsColored(1));
+        }
+    }
+
+    private CommandSender findReceiver(String input) {
+        if (input.equalsIgnoreCase("console")) {
+            return server.getConsoleSender();
+        } else if (UUIDHelper.isValidUUID(input)) {
+            return playerService.findOnlinePlayer(UUIDHelper.getFromString(input))
+                    .orElseThrow(() -> new I18nUserException("error!player-not-on"));
+        } else {
+            return playerService.findOnlinePlayer(input)
+                    .orElseThrow(() -> new I18nUserException("error!no-such-player"));
+        }
     }
 }
