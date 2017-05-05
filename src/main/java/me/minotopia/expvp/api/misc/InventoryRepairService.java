2dfc0e3cef7ebc12562d9964b8d8961839410506
Literallie <git@l1t.li>
2017-05-05 21:08:44 +0200

    Log repair service actions

--- src/main/java/me/minotopia/expvp/api/misc/InventoryRepairService.java
+++ src/main/java/me/minotopia/expvp/api/misc/InventoryRepairService.java
@@ -8,31 +8,45 @@
 
 package me.minotopia.expvp.api.misc;
 
-import com.google.common.collect.Sets;
+import me.minotopia.expvp.logging.LoggingManager;
+import org.apache.logging.log4j.Logger;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
-
-import java.util.Set;
+import org.bukkit.inventory.PlayerInventory;
 
 /**
  * @author <a href="https://l1t.li/">Literallie</a>
  * @since 2017-05-05
  */
 public class InventoryRepairService implements RepairService {
-    private final Set<Material> repairable = Sets.newHashSet(
-            Material.LEATHER_HELMET, Material.LEATHER_CHESTPLATE, Material.LEATHER_LEGGINGS, Material.LEATHER_BOOTS,
-            Material.WOOD_SWORD, Material.BOW, Material.WOOD_AXE, Material.WOOD_SPADE
-    );
-
+    private final Logger LOGGER = LoggingManager.getLogger(InventoryRepairService.class);
     @Override
     public void repair(Player player) {
-        for (int i = 0; i < player.getInventory().getSize(); i++) {
-            ItemStack item = player.getInventory().getItem(i);
-            if (item != null && repairable.contains(item.getType())) {
+        PlayerInventory inv = player.getInventory();
+        for (int i = 0; i < inv.getSize(); i++) {
+            ItemStack item = inv.getItem(i);
+            if (item != null && isRepairable(item.getType())) {
                 item.setDurability(item.getType().getMaxDurability());
-                player.getInventory().setItem(i, item);
+                inv.setItem(i, item);
+                LOGGER.debug("Repairing {} - {} to {}", player.getName(), item.getType(), item.getDurability());
             }
+        }
+    }
+
+    private boolean isRepairable(Material material) {
+        switch (material) {
+            case LEATHER_HELMET:
+            case LEATHER_CHESTPLATE:
+            case LEATHER_LEGGINGS:
+            case LEATHER_BOOTS:
+            case WOOD_SWORD:
+            case WOOD_SPADE:
+            case WOOD_AXE:
+            case BOW:
+                return true;
+            default:
+                return false;
         }
     }
 }
